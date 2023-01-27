8bc0e50364e1c89964a563dd31a2ea1c26b4201f
rabby420 <mahedihassanrabby420@gmail.com>
2023-01-27 08:19:10 +0600

    another two algorithm added

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-# This is a sample Python script.
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 25 15:24:52 2023
+
+@author: Rabby
+"""
+
 import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import pickle 
 
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
+from sklearn import linear_model
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import confusion_matrix,classification_report,accuracy_score
 # Press Shift+F10 to execute it or replace it with your code.
@@ -29,19 +36,98 @@ def preprocess(df):
 
         df[column] = pd.Series(col_values)
     return df
+def svm(X,Y,df):
+    #st.header("You are fully well , don't worry ")
+    loaded_model=pickle.load(open('D:/ai_project/svm_model.sav','rb'))
+    input=df.to_numpy()
+    input_reshaped=input.reshape(1,-1)
+    prediction=loaded_model.predict(input_reshaped)
+    st.write(prediction)
+    if prediction[0]==1:
+        st.header("Result- This patient is diabetic")
+    else:
+        st.write("Using Svm-")
+        st.header("Result-This patient is not diabetic")
+    model = RandomForestRegressor()
+    model.fit(X, Y)
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(X)
+    st.write('---')
+    st.header('Feature Importance')
+    plt.title('Feature importance based on SHAP values')
+    shap.summary_plot(shap_values, X)
+    st.pyplot(bbox_inches='tight')
+    st.write('---')
+    plt.title('Feature importance based on SHAP values (Bar)')
+    shap.summary_plot(shap_values, X, plot_type="bar")
+    st.pyplot(bbox_inches='tight')
+    print(df.describe())
+    
+def logistic_regression(X,Y,df):
+    logr=linear_model.LogisticRegression()
+    logr.fit(X,Y)
+    input=df.to_numpy()
+    input_reshaped=input.reshape(1,-1)
+    predicted=logr.predict(input_reshaped)
+    st.write(predicted)
+    if predicted[0]==1:
+        st.header("The patient is diabetic")
+    else:
+        st.header("The patient is not diabetic")
+    model = RandomForestRegressor()
+    model.fit(X, Y)
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(X)
+    st.header('Feature Importance')
+    plt.title('Feature importance based on SHAP values')
+    shap.summary_plot(shap_values, X)
+    st.pyplot(bbox_inches='tight')
+    st.write('---')
+    plt.title('Feature importance based on SHAP values (Bar)')
+    shap.summary_plot(shap_values, X, plot_type="bar")
+    st.pyplot(bbox_inches='tight')
+    print(df.describe())
+    
+def random_forest(X,Y,df):
+    # Build Regression Model
+    model = RandomForestRegressor()
+    model.fit(X, Y)
+    # Apply Model to Make Prediction
+    prediction = model.predict(df)
 
+    st.header('Prediction of Outcome')
+    st.write(prediction)
+    st.write('---')
+
+    # Explaining the model's predictions using SHAP values
+    # https://github.com/slundberg/shap
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(X)
+
+    st.header('Feature Importance')
+    plt.title('Feature importance based on SHAP values')
+    shap.summary_plot(shap_values, X)
+    st.pyplot(bbox_inches='tight')
+    st.write('---')
+
+    plt.title('Feature importance based on SHAP values (Bar)')
+    shap.summary_plot(shap_values, X, plot_type="bar")
+    st.pyplot(bbox_inches='tight')
+    print(df.describe())  # Press Ctrl+F8 to toggle the breakpoint.
+    
+    
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     st.write("""
     # Diabetes Prediction App (AI Project #Team 8)
     This app predicts  **Diabetes**! You can give your custom input in sidebar.
-    We used *Random Forest Model* to predict **Diabetes**
+    We used different model to predict **Diabetes**
     """)
     st.write('---')
 
     # Loads the Boston House Price Dataset
 
-    df = pd.read_csv('./diabetes.csv')
+    df = pd.read_csv('D:/ai_project/diabetes.csv')
     df = preprocess(df)
     X = df.drop('Outcome', axis=1)
     Y = df['Outcome']
@@ -73,36 +159,24 @@ def print_hi(name):
     # Main Panel
 
     # Print specified input parameters
-    st.header('Specified Input parameters')
     st.write(df)
     st.write('---')
-
-    # Build Regression Model
-    model = RandomForestRegressor()
-    model.fit(X, Y)
-    # Apply Model to Make Prediction
-    prediction = model.predict(df)
-
-    st.header('Prediction of Outcome')
-    st.write(prediction)
-    st.write('---')
-
-    # Explaining the model's predictions using SHAP values
-    # https://github.com/slundberg/shap
-    explainer = shap.TreeExplainer(model)
-    shap_values = explainer.shap_values(X)
-
-    st.header('Feature Importance')
-    plt.title('Feature importance based on SHAP values')
-    shap.summary_plot(shap_values, X)
-    st.pyplot(bbox_inches='tight')
-    st.write('---')
-
-    plt.title('Feature importance based on SHAP values (Bar)')
-    shap.summary_plot(shap_values, X, plot_type="bar")
-    st.pyplot(bbox_inches='tight')
-    print(df.describe())  # Press Ctrl+F8 to toggle the breakpoint.
-
+    
+    
+    #option for different algorithm
+    st.header("Select specific algorithm")
+    option = st.selectbox(
+    '',('Random forest', 'Svm','Logistic Regression'))
+    #st.write('You selected:', option)
+    if option=="Random forest":
+        random_forest(X,Y,df)
+    elif option=="Svm":
+        svm(X,Y,df)
+    elif option=="Logistic Regression":
+        logistic_regression(X,Y,df);
+    
+    
+    #to this
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
